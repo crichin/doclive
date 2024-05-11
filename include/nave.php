990e210b57562ac14b75301ffdf13884ae2cccdc
Eurin HASH <eurindelagoldcoast@gmail.com>
2024-05-11 12:33:48 +0100

    New CSS

--- include/nave.php
+++ include/nave.php
@@ -1,7 +1,13 @@
-<nav class="navbar bg-gray-900 p-4">
-    <ul class="menu flex items-center justify-center space-x-4">
-        <li><a href="index.php" class="text-white font-bold">Doclive</a></li>
-        <li><a href="connexion.php" class="text-white">Connexion</a></li>
-        <li><a href="inscription.php" class="text-white">Inscription</a></li>
+<nav class="navbar">
+    <ul class="menu">
+        <li><a href="index.php">Doclive</a></li>
+        <li><a href="connexion.php">Connexion</a></li>
+        <li><a href="inscription.php">Inscription</a></li>
+        <li><a href="#">Autre lien</a></li>
     </ul>
+    <div class="hamburger-menu" onclick="toggleMenu()">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+    </div>
 </nav>
